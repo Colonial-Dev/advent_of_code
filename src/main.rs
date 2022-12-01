bdba377672debe68b7c80094444617a5e9e426d4
SomewhereOutInSpace <jameshaywood@fastmail.com>
2022-12-01 02:28:20 -0500

    Completed day one

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![warn(clippy::perf, clippy::style, warnings)]
+
+fn calorie_counting() {
+    let top_three = include_str!("inputs/01.txt")
+        .split("\n\n")
+        .map(|set| {
+            set.lines()
+                .map(str::parse::<u64>)
+                .map(Result::unwrap)
+                .sum::<u64>()
+        })
+        .fold([0, 0, 0], |mut acc, n| {
+            for value in &mut acc {
+                if n > *value {
+                    *value = n;
+                    break;
+                }
+            }
+            acc
+        });      
+        
+    let max = top_three[0];
+    let sum = top_three.iter().sum::<u64>();
+    
+    println!("The highest number of calories carried by any individual elf is {max}.");
+    println!("The total number of calories carried by the top three load-bearing elves is {sum}.");
+}
+
+fn main() {
+    calorie_counting()  
+}
