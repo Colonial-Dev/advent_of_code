623fd82d86a202b1c67561be2efbf4f95abef0cf
SomewhereOutInSpace <jameshaywood@fastmail.com>
2022-12-03 01:08:23 -0500

    Completed day 3

---
+++ src/day_03.rs
@@ -0,0 +1,49 @@
+pub fn solution() {
+    let input: Vec<_> = include_str!("inputs/03.txt").lines().collect();
+
+    let sum = input.iter()
+        .map(|line| {
+            let end = line.len();
+            let midpoint = end / 2;
+            [&line[0..midpoint], &line[midpoint..end]]
+        })
+        .map(find_array_intersection)
+        .map(priority_codes)
+        .sum::<u64>();
+
+    let group_sum = input.chunks(3)
+        .map(<[&str; 3]>::try_from)
+        .map(Result::unwrap)
+        .map(find_array_intersection)
+        .map(priority_codes)
+        .sum::<u64>();
+    
+    println!("The priority sum is {sum}.");
+    println!("The group sum is {group_sum}.");
+}
+
+fn find_array_intersection<const N: usize>(set: [&str; N]) -> Vec<char> {
+    let mut intersection = set[0]
+        .chars()
+        .filter(|char| {
+            for item in set.iter().skip(1) {
+                if !item.contains(*char) { return false; }
+            }
+            true
+        })
+        .collect::<Vec<char>>();
+
+    intersection.sort_unstable();
+    intersection.dedup();
+    intersection
+}
+
+fn priority_codes(characters: Vec<char>) -> u64 {
+    characters
+        .into_iter()
+        .map(|character| match character.is_uppercase() {
+            true => (character as u8 - 65) + 27,
+            false => character as u8 - 96
+        } as u64)
+        .sum::<u64>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+#![allow(dead_code)]
 #![warn(clippy::perf, clippy::style, warnings)]
 
 mod day_01;
 mod day_02;
+mod day_03;
 
 fn main() {
-    day_02::solution()  
+    day_03::solution();
 }
